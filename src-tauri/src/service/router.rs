2b1bc25373d1cfff1b7ec090e08a65a24ed9b09d
ShirasawaTopaz <3489840593@qq.com>
2025-07-25 17:22:36 +0800

    feat(service): 重构 Router 并添加新功能
    - 为 Router 实现 Default trait
    - 新增 attach_route 方法用于添加路由
    - 新增 dispatch 方法用于路由分发
    - 优化了路由映射的类型定义

--- src-tauri/src/service/router.rs
+++ src-tauri/src/service/router.rs
@@ -3,10 +3,31 @@ use std::future::Future;
 use std::pin::Pin;
 use std::sync::Arc;
 
-#[allow(clippy::type_complexity)]
+pub type FunctionPointer =
+    Arc<dyn Fn(String) -> Pin<Box<dyn Future<Output = String> + Send + Sync>>>;
+
 pub struct Router {
-    pub route_map: HashMap<
-        String,
-        Arc<dyn Fn(String) -> Pin<Box<dyn Future<Output = String> + Send + Sync>> + Send + Sync>,
-    >,
+    route_map: HashMap<String, FunctionPointer>,
+}
+
+impl Default for Router {
+    fn default() -> Self {
+        Self::new()
+    }
+}
+
+impl Router {
+    pub fn new() -> Self {
+        Router {
+            route_map: HashMap::new(),
+        }
+    }
+
+    pub fn attach_route(&mut self, route: String, function: FunctionPointer) {
+        self.route_map.insert(route, function);
+    }
+
+    pub async fn dispatch(&self, path: String) -> Option<&FunctionPointer> {
+        self.route_map.get(&path)
+    }
 }
