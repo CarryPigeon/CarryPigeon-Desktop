f9d1a917acfa0112883614fb43f2637de95cf547
ShirasawaTopaz <34898440593@qq.com>
2025-08-18 09:26:50 +0800

    refactor(encryption): 重构加密模块
    - 移除了 rsa 和 pem 依赖
    - 替换了 RSA 加密方式，使用 AES-256 代替
    -简化了 Encryption 结构体，移除了 rsa_public_key 字段
    - 更新了 Encryption 实现，支持 AES 密钥解析和使用

--- src-tauri/src/controller/encryption.rs
+++ src-tauri/src/controller/encryption.rs
@@ -1,22 +1,22 @@
+use aes::{Aes256, Aes256Enc};
+use aes::cipher::KeyInit;
 use crate::service::net::tcp_service::TCP_SERVICE;
 use base64::encode;
 use ring::agreement::{PublicKey, ECDH_P256};
 use ring::rand::SystemRandom;
 use ring::signature::{EcdsaKeyPair, KeyPair};
 use ring::{agreement, signature};
-use rsa::pkcs1::DecodeRsaPublicKey;
-use rsa::RsaPublicKey;
 
 #[derive(Debug)]
 pub struct Encryption {
     ecc_private_key: EcdsaKeyPair,
-    rsa_public_key: RsaPublicKey,
+    aes_key: String,
 }
 
 impl Encryption {
     pub async fn new() -> Self {
         let ecc_private_key = Encryption::crate_ecc().await;
-        Self { ecc_private_key }
+        Self { ecc_private_key, aes_key: String::new() }
     }
     pub async fn crate_ecc() -> EcdsaKeyPair {
         let rng = SystemRandom::new();
@@ -37,17 +37,16 @@ impl Encryption {
 
         private_key
     }
-    pub async fn parse_rsa(&mut self, pem_str:String) {
-        let pem = pem::parse(pem_str).unwrap();
-        let public_key = RsaPublicKey::from_pkcs1_der(&pem.contents()).unwrap();
-        self.rsa_public_key = public_key;
+
+    pub fn prase_aes(&mut self, key:String) {
+
     }
     pub async fn send_verification(&self, message: String, rsa: &Vec<u8>) {
         unsafe {
             TCP_SERVICE
                 .get_mut()
                 .unwrap()
-                .send_message(encode(rsa))
+                .send_message(encode(Aes256Enc::))
                 .await
                 .unwrap()
         }
